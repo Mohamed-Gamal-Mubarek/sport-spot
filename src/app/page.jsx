07a36487881829d95ff8c4ccf6df725e3368a45e
Mohamed-Gamal-Mubarek <88108594+Mohamed-Gamal-Mubarek@users.noreply.github.com>
2025-04-22 13:14:26 +0200

    feat : home page finsih with data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,18 @@
 import PicAndTitle from "@/components/pic-and-title";
 import homePic from "@/assets/imgs/home/home.jpg"
+import { home } from "@/data/home"
+import Card from "@/components/card"
 const Home = () => {
   return (
 
     <>
       <title>Home</title>
       <PicAndTitle title={'home'} pic={homePic} />
-      <div className="container bg-[red] mx-auto mt-5">
-        
+      {/* start page  */}
+      <div className="container mx-auto my-5">
+        <div className="grid grid-cols-1 gap-5">
+          {home.map((news, i) => (<Card {...news} key={i} />))}
+        </div>
       </div>
     </>
   );
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -5,12 +5,12 @@ const Card = ({ pic, title, description }) => {
         <>
             <div className="border border-[gray] overflow-hidden  rounded-3xl">
                 {/* pic */}
-                <div className="relative w-full h-64 md:h-96 overflow-hidden rounded-3xl">   {/* parent */}
+                <div className="relative w-full h-64 md:h-96">   {/* parent */}
                     <Image
                         src={pic}
                         alt="Hero banner"
                         fill                    /* makes img absolutely positioned */
-                        className="object-cover"/* or object-contain */
+                        className="object-fill"/* or object-contain */
                         priority                /* optional: pre‑load above‑the‑fold */
                     />
                 </div>
@@ -18,10 +18,10 @@ const Card = ({ pic, title, description }) => {
                 {/* content */}
                 <div className="px-4 py-4">
 
-                    <h2 className="text-[30px] font-bold">
+                    <h2 className="text-[30px] font-bold text-end">
                         {capitalizeFirst(title)}
                     </h2>
-                    <p className="mt-2 placeholder:text-gray-400 ">
+                    <p className="mt-2 placeholder:text-gray-400 text-end">
                         {description}
                     </p>
                 </div>
