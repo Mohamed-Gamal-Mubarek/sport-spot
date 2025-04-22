107d255f29c2a33d9a2160f00c1057c7905a9216
Mohamed-Gamal-Mubarek <88108594+Mohamed-Gamal-Mubarek@users.noreply.github.com>
2025-04-22 13:50:21 +0200

    feat : news page is finished with data

--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,12 +1,19 @@
 import PicAndTitle from "@/components/pic-and-title";
 import newsPic from "@/assets/imgs/news/news.jpeg"
+import { news } from "@/data/news"
+import Card from "@/components/card"
 const News = () => {
     return (
 
         <>
             <title>News</title>
             <PicAndTitle title={'news'} pic={newsPic} />
-            <h1>new page will be in here</h1>
+            {/* start page  */}
+            <div className="container mx-auto my-5">
+                <div className="grid grid-cols-1 gap-5">
+                    {news.map((cardNews, i) => (<Card {...cardNews} key={i} />))}
+                </div>
+            </div>
         </>
     );
 }
